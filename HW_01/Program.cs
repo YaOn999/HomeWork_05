da3f8246e5ce6e55fe760a0fc3a10f1c08e249f8
YaOn999 <lumous999@gmail.com>
2022-12-15 20:51:07 +0300

    New HW_01

---
+++ HW_01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int EvenNumber(int[] array)
+{
+    int amount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) amount = amount + 1;
+    }
+    return amount;
+}
+
+int[] NewMassive(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max++);
+    }
+    return array;
+}
+
+int[] array = NewMassive(10, 100, 999);
+Console.WriteLine($"в массиве [{String.Join(",", array)}]");
+
+int rezult = EvenNumber(array);
+Console.WriteLine($"{rezult} четных чисел");
